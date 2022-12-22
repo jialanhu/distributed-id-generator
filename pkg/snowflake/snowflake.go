cb5ab65ed9abed820e3f10cfb66df92e3d08cbae
JiaLan <sakaketsukihana@github.com>
2022-12-23 01:27:31 +0800

    support options

--- pkg/snowflake/snowflake.go
+++ pkg/snowflake/snowflake.go
@@ -7,19 +7,52 @@ import (
 	"time"
 )
 
-const (
-	Epoch int64 = 1671698366302
+type options struct {
+	epoch    int64
+	timeBits uint8
+	nodeBits uint8
+	stepBits uint8
+}
 
-	TimeBits uint8 = 41
-	NodeBits uint8 = 10 // 0 ~ 1023
-	StepBits uint8 = 12 // 0 ~ 4095 => 4096/ms
+type optionsFunc func(*options) error
 
-	NodeMax int64 = -1 ^ (-1 << NodeBits)
-)
+// Milliseconds
+func SetEpoch(epoch int64) optionsFunc {
+	return func(o *options) error {
+		if epoch > time.Now().UnixMilli() {
+			return errors.New("cannot be in the future")
+		}
+		o.epoch = epoch
+		return nil
+	}
+}
+
+func CustomBit(timeBits, nodeBits, stepBits uint8) optionsFunc {
+
+	return func(o *options) error {
+		if timeBits+nodeBits+stepBits != 63 || timeBits == 0 || nodeBits == 0 || stepBits == 0 {
+			return errors.New("you have a total 63 bits to share between Time/Node/Step")
+		}
+		o.timeBits = timeBits
+		o.nodeBits = nodeBits
+		o.stepBits = stepBits
+		return nil
+	}
+}
+
+var defaultOptions = &options{
+	epoch: 1671698366302, // 2022-12-22 16:39:26
+
+	timeBits: 41,
+	nodeBits: 10, // 0 ~ 1023
+	stepBits: 12, // 0 ~ 4095 => 4096/ms
+}
 
 type Node struct {
-	mu    sync.Mutex
-	epoch time.Time
+	mu sync.Mutex
+
+	epoch     int64
+	epochTime time.Time
 
 	time int64
 	node int64
@@ -34,36 +67,44 @@ type Node struct {
 
 type ID int64
 
-func NewNode(node int64) (*Node, error) {
-	if TimeBits+NodeBits+StepBits > 63 {
-		return nil, errors.New("you have a total 63 bits to share between Time/Node/Step")
+func NewNode(node int64, optFuns ...optionsFunc) (*Node, error) {
+	opts := defaultOptions
+	for _, optFun := range optFuns {
+		err := optFun(opts)
+		if err != nil {
+			return nil, err
+		}
 	}
-	// todo node 重复
-	if node < 0 || node > NodeMax {
-		return nil, errors.New("node number must be between 0 and " + strconv.FormatInt(NodeMax, 10))
-	}
-	return &Node{
-		epoch: time.Unix(Epoch/1000, Epoch%1000*1000000),
-		node:  node,
 
-		timeShift: NodeBits + StepBits,
-		nodeShift: StepBits,
-		nodeMax:   ^(-1 << NodeBits),
-		nodeMask:  NodeMax << StepBits,
-		stepMask:  ^(-1 << StepBits),
-	}, nil
+	n := &Node{
+		node: node,
+
+		epoch:     opts.epoch,
+		epochTime: time.Unix(opts.epoch/1000, opts.epoch%1000*1000000),
+
+		timeShift: opts.nodeBits + opts.stepBits,
+		nodeShift: opts.stepBits,
+		nodeMax:   ^(-1 << opts.nodeBits),
+		nodeMask:  ^(-1 << opts.nodeBits) << opts.stepBits,
+		stepMask:  ^(-1 << opts.stepBits),
+	}
+
+	if node < 0 || node > n.nodeMax {
+		return nil, errors.New("node number must be between 0 and " + strconv.FormatInt(n.nodeMax, 10))
+	}
+	return n, nil
 }
 
 func (n *Node) GenerateID() ID {
 	n.mu.Lock()
 	defer n.mu.Unlock()
 
-	now := time.Since(n.epoch).Milliseconds()
+	now := time.Since(n.epochTime).Milliseconds()
 	if now <= n.time { // 防止时钟回拨异常
 		n.step = (n.step + 1) & n.stepMask
 		if n.step == 0 {
 			for now <= n.time {
-				now = time.Since(n.epoch).Milliseconds()
+				now = time.Since(n.epochTime).Milliseconds()
 			}
 		}
 	} else {
@@ -75,7 +116,7 @@ func (n *Node) GenerateID() ID {
 }
 
 func (n *Node) ParseMSTime(id ID) int64 {
-	return int64(id)>>n.timeShift + Epoch
+	return int64(id)>>n.timeShift + n.epoch
 }
 
 func (n *Node) ParseNodeID(id ID) int64 {
@@ -87,5 +128,5 @@ func (n *Node) ParseStep(id ID) int64 {
 }
 
 func (n *Node) getTime() int64 {
-	return n.time + Epoch
+	return n.time + n.epoch
 }
--- pkg/snowflake/snowflake_test.go
+++ pkg/snowflake/snowflake_test.go
@@ -1,22 +1,25 @@
 package snowflake
 
-import "testing"
+import (
+	"testing"
+	"time"
+)
 
 func TestNewNode(t *testing.T) {
 	_, err := NewNode(0)
 	if err != nil {
-		t.Fatalf("error creating NewNode, %s", err)
+		t.Errorf("error creating NewNode, %s", err)
 	}
 
 	_, err = NewNode(1024)
 	if err == nil {
-		t.Fatalf("no error creating NewNode, %s", err)
+		t.Errorf("no error creating NewNode")
 	}
 }
 
 func TestGenerateID(t *testing.T) {
 
-	node, _ := NewNode(1)
+	node, _ := NewNode(0)
 
 	var old, new ID
 	for i := 0; i < 1000000; i++ {
@@ -33,11 +36,11 @@ func TestGenerateID(t *testing.T) {
 }
 
 func TestParse(t *testing.T) {
-	node, _ := NewNode(2)
+	node, _ := NewNode(0)
 	id := node.GenerateID()
 	nodeID := node.ParseNodeID(id)
-	if nodeID != 2 {
-		t.Errorf("ParseNodeID error nodeID:(%d) want:(2)", nodeID)
+	if nodeID != 0 {
+		t.Errorf("ParseNodeID error nodeID:(%d) want:(0)", nodeID)
 	}
 	step := node.ParseStep(id)
 	if step != 0 {
@@ -47,5 +50,62 @@ func TestParse(t *testing.T) {
 	nodeTime := node.getTime()
 	if time != nodeTime {
 		t.Errorf("ParseMSTime error time:(%d) want:(%d)", time, nodeTime)
+	}
+}
+
+func TestCustomBit(t *testing.T) {
+	// node timeBits nodeBits stepBits result(err == nil => 0)
+	useCase := [][]int{
+		{0, 21, 21, 21, 0},
+		{255, 39, 8, 16, 0},
+
+		// error case
+		{0, 1, 2, 3, 1},
+		{256, 39, 8, 3, 1},
+		{0, 0, 32, 31, 1},
+		{0, 31, 0, 32, 1},
+		{0, 31, 32, 0, 1},
+	}
+
+	for _, uc := range useCase {
+		_, err := NewNode(int64(uc[0]), CustomBit(uint8(uc[1]), uint8(uc[2]), uint8(uc[3])))
+		if (err != nil && uc[4] == 0) || (err == nil && uc[4] == 1) {
+			t.Errorf("error NewNode CustomBit, case %v err %s", uc, err)
+		}
+	}
+}
+
+func TestSetEpoch(t *testing.T) {
+	_, err := NewNode(0, SetEpoch(time.Now().UnixMilli()+1000))
+	if err == nil {
+		t.Errorf("no error NewNode SetEpoch")
+	}
+	_, err = NewNode(0, SetEpoch(time.Now().UnixMilli()))
+	if err != nil {
+		t.Errorf("error NewNode SetEpoch, %s", err)
+	}
+}
+
+func BenchmarkGenerate(b *testing.B) {
+
+	node, _ := NewNode(0)
+
+	b.ReportAllocs()
+
+	b.ResetTimer()
+	for n := 0; n < b.N; n++ {
+		_ = node.GenerateID()
+	}
+}
+
+func BenchmarkGenerateMaxSequence(b *testing.B) {
+
+	node, _ := NewNode(0, CustomBit(32, 1, 30))
+
+	b.ReportAllocs()
+
+	b.ResetTimer()
+	for n := 0; n < b.N; n++ {
+		_ = node.GenerateID()
 	}
 }
